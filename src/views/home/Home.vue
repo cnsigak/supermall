3dae75fc29110a57c25ab4c223fa1c222474d6fc
C-data <405693156@qq.com>
2020-12-22 17:52:43 +0800

    message

---
+++ src/views/home/Home.vue
@@ -0,0 +1,174 @@
+<template>
+  <div id="home">
+    <!-- 导航栏 -->
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <!-- 轮播图 -->
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+	import NavBar from 'components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+  import RecommendView from './childComps/RecommendView.vue'
+  import FeatureView from './childComps/FeatureView.vue'
+
+  import TabControl from 'components/content/tabControl/TabControl.vue'
+
+  import {getHomeMultidata} from "network/home.js"
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        titles: []
+      }
+    },
+    created() {
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+        console.log(res.data)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
+</style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="featrue">
+    <a href="https://act.moguijie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .featrue img {
+    width: 100%;
+  }
+</style>
